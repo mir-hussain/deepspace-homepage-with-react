f8e7a3e72ff27b18b77310aa743645d94f4356c7
mir-hussain <mirhussainmurtaza@gmail.com>
2022-06-15 21:21:03 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className='flex justify-center items-center h-screen bg-dark-blue bg-main bg-contain'>
+      <h1>init</h1>
+    </div>
+  );
+}
+
+export default App;
