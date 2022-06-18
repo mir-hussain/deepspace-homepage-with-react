dc011349ff7f13cbd2e41c3d9c856362a5803e61
mir-hussain <mirhussainmurtaza@gmail.com>
2022-06-18 23:48:23 +0600

    working on partner section

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Navbar from "../../components/Navbar";
 import Landing from "./Landing";
+import Partners from "./Partners";
 
 const Home = () => {
   return (
     <>
       <Navbar />
       <Landing />
+      <Partners />
     </>
   );
 };
---
+++ src/pages/home/Partners.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Partners = () => {
+  return (
+    <div className='py-10'>
+      <h1 className='text-center text-xl text-slate-400'>
+        3,000+ companies trust DeepSource to ship clean and secure code
+      </h1>
+    </div>
+  );
+};
+
+export default Partners;
