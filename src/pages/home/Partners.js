e4e751adc24ad43cd862d375b2b4d4d7bacdda61
mir-hussain <mirhussainmurtaza@gmail.com>
2022-06-20 21:56:54 +0600

    working on logos

--- src/pages/home/Partners.js
+++ src/pages/home/Partners.js
@@ -2,10 +2,17 @@ import React from "react";
 
 const Partners = () => {
   return (
-    <div className='py-10'>
+    <div className='py-10 bg-dark-blue my-20'>
       <h1 className='text-center text-xl text-slate-400'>
         3,000+ companies trust DeepSource to ship clean and secure code
       </h1>
+      <div>
+        <dir></dir>
+        <dir></dir>
+        <dir></dir>
+        <dir></dir>
+        <dir></dir>
+      </div>
     </div>
   );
 };
