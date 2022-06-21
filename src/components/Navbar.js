253014e1f66d4be6494d6e798eef109d2bd8ad92
mir-hussain <mirhussainmurtaza@gmail.com>
2022-06-21 17:19:48 +0600

    basic gsap setup done

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,26 +1,39 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import Logo from "../assets/images/logo.svg";
+import { gsap } from "gsap";
+
 const Navbar = () => {
+  let navRef = useRef(null);
+
+  useEffect(() => {
+    gsap.to(navRef, { opacity: 0 });
+  }, []);
+
   return (
-    <header class='sticky top-0 z-20'>
-      <nav class='navbar flex justify-center items-center relative z-50 gap-16 h-16 text-white text-lg font-medium'>
-        <img class='h-7 cursor-pointer' src={Logo} alt='' />
-        <ul class='flex gap-8'>
-          <li class='cursor-pointer'>Products</li>
-          <li class='cursor-pointer'>Enterprise</li>
-          <li class='cursor-pointer'>Pricing</li>
-          <li class='cursor-pointer'>Resources</li>
-          <li class='cursor-pointer'>Company</li>
-          <li class='cursor-pointer'>Jobs</li>
+    <header
+      ref={(el) => {
+        navRef = el;
+      }}
+      className='sticky top-0 z-20'
+    >
+      <nav className='navbar flex justify-center items-center relative z-50 gap-16 h-16 text-white text-lg font-medium'>
+        <img className='h-7 cursor-pointer' src={Logo} alt='' />
+        <ul className='flex gap-8'>
+          <li className='cursor-pointer'>Products</li>
+          <li className='cursor-pointer'>Enterprise</li>
+          <li className='cursor-pointer'>Pricing</li>
+          <li className='cursor-pointer'>Resources</li>
+          <li className='cursor-pointer'>Company</li>
+          <li className='cursor-pointer'>Jobs</li>
         </ul>
-        <ul class='flex items-center gap-8'>
-          <li class='cursor-pointer'>Log in</li>
-          <li class='cursor-pointer bg-green px-3 py-1 rounded-sm text-dark'>
+        <ul className='flex items-center gap-8'>
+          <li className='cursor-pointer'>Log in</li>
+          <li className='cursor-pointer bg-green px-3 py-1 rounded-sm text-dark'>
             Sign up
           </li>
         </ul>
       </nav>
-      <div class='navbar-background bg-black bg-opacity-50 backdrop-blur-md h-16 w-full absolute top-0'></div>
+      <div className='navbar-background bg-black bg-opacity-50 backdrop-blur-md h-16 w-full absolute top-0'></div>
     </header>
   );
 };
